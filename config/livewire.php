d86f8e167f157149951d8c8d431fe6b34fdd7d17
Jose Anicama <j.j.anicamatanta@gmail.com>
2026-01-12 15:55:49 -0500

    Update Livewire configuration to set correct update path and register CustomHandleRequests

--- config/livewire.php
+++ config/livewire.php
@@ -4,7 +4,7 @@ return [
 
     'asset_url' => env('LIVEWIRE_ASSET_URL', env('APP_URL') . '/livewire/livewire.js'),
     'app_url' => env('LIVEWIRE_APP_URL', env('APP_URL')),
-    'update_path' => env('LIVEWIRE_UPDATE_PATH', 'education_portal/livewire/update'),
+    'update_path' => env('LIVEWIRE_UPDATE_PATH', 'portfolio_backend/livewire/update'),
 
 
     /*
